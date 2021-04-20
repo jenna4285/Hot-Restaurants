e4afca61f70a5c1c712f349aff30460c92142d3d
hamzsait <hamzasait@utexas.edu>
2021-04-20 14:52:22 -0500

    added package.json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 // Sets up the Express App
 const app = express();
-const PORT = 3000;
+const PORT = process.env.port || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
