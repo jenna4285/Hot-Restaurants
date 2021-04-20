d46a18379210e0be9dd6083bb8a21c1f7ccea338
hamzsait <hamzasait@utexas.edu>
2021-04-20 14:13:39 -0500

    fixed the waiting list

--- server.js
+++ server.js
@@ -22,10 +22,36 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'add.html')));
 app.get('/view', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
 // Displays all customers
-app.get('/api/tables', (req, res) => res.json(customers));
+app.get('/api/tables', (req, res) =>{
+    output = []
+    if (customers.length > 5){
+        for (x = 0; x < 5; x++){
+            output.push(customers[x])
+        }
+    }
+    else{
+        for(customer of customers){
+            output.push(customer)
+        }
+    }
+    res.json(output)
+});
+
+app.get('/api/waitlist', (req, res) => {
+    output = []
+    if (customers.length>5){
+        for(x = 5;x < customers.length;x++){
+            output.push(customers[x])
+        }
+    }
+    res.json(output)
+});
+
+
 // Create New Customer - takes in JSON input
 app.post('/api/tables', (req, res) => {
   customers.push(req.body)
 });
+
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
